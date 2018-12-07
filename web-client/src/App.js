209344cad459c3146eb7ae268e6f52e94b37f5a6
samcyn <samsoniyanda@outlook.com>
2018-12-07 16:59:52 +0100

    set up client side of the application

---
+++ web-client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        ADD
+      </div>
+    );
+  }
+}
+
+export default App;
